67c78f4c0c478cd10e87b5819a417f03b8eeeff9
xiaolu00185 <150724036@qq.com>
2019-01-04 14:37:53 +0800

    reset rsponse

---
+++ config/index.js
@@ -0,0 +1,82 @@
+'use strict'
+// Template version: 1.2.8
+// see http://vuejs-templates.github.io/webpack for documentation.
+
+const path = require('path')
+
+const processEnv = process.env.NODE_ENV;
+const envConfig = process.env.ENV_CONFIG;//ngx||devProxy
+let buildFolder= (envConfig && envConfig == 'ngx')
+								?'f:\\2018\\****\\dist\\static'
+								:'F:\\java\\workspace\\2018\\****\\main\\resources';
+
+
+module.exports = {
+  dev: {
+
+    // Paths
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: (envConfig && envConfig == 'devProxy') ? {
+      '**/*.action': {
+        target: 'http://127.0.0.1:8092',
+        changeOrigin: true,
+        logLevel: 'debug'
+      }
+    } : {},
+    // Various Dev Server settings
+    host: 'localhost', // can be overwritten by process.env.HOST
+    port: 8086, //
+
+
+    autoOpenBrowser: true,
+    errorOverlay: true,
+    notifyOnErrors: true,
+    poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+
+
+    /**
+     * Source Maps
+     */
+
+    // https://webpack.js.org/configuration/devtool/#development
+    devtool: 'eval-source-map',
+
+    // If you have problems debugging vue-files in devtools,
+    // set this to false - it *may* help
+    // https://vue-loader.vuejs.org/en/options.html#cachebusting
+    cacheBusting: false,
+
+    cssSourceMap: true,
+  },
+
+  build: {
+    // Template for index.html
+    index:(envConfig && envConfig == 'ngx')
+						? `${buildFolder}\\index.html`
+						: `${buildFolder}\\templates\\pages\\index.html`,
+    assetsRoot: buildFolder,
+    assetsSubDirectory: 'static',
+    assetsPublicPath: (envConfig && envConfig == 'ngx') ? './' : '../../',
+    /**
+     * Source Maps
+     */
+
+    productionSourceMap: false,
+    // https://webpack.js.org/configuration/devtool/#production
+    devtool: '#source-map',
+
+    // Gzip off by default as many popular static hosts such as
+    // Surge or Netlify already gzip all static assets for you.
+    // Before setting to `true`, make sure to:
+    // npm install --save-dev compression-webpack-plugin
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
+
+    // Run the build command with an extra argument to
+    // View the bundle analyzer report after build finishes:
+    // `npm run build --report`
+    // Set to `true` or `false` to always turn it on or off
+    bundleAnalyzerReport: process.env.npm_config_report
+  }
+}
